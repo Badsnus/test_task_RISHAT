2b6b59708b4f90ec1aae1a1bb2ef6d25a52ebaa8
Badsnus <anton.098.bal@gmail.com>
2023-02-21 15:50:45 +0500

    added Item model

--- shop/products/models.py
+++ shop/products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField('название', max_length=300)
+    description = models.CharField('описание', max_length=2000)
+    price = models.PositiveIntegerField('цена')
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
